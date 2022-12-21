2e912392a60433365dc57193ebfa6de7d8bbd53a
Agnes Beviz <Agnes.Beviz@dvla.gov.uk>
2022-12-21 17:34:28 +0000

    Turns: added logic to prompt a player to select a different column if they choose a full column

--- src/main/java/org/example/Turn/Turn.java
+++ src/main/java/org/example/Turn/Turn.java
@@ -7,12 +7,20 @@ public class Turn {
 
     public static void playerTakesTurn (Player player, int[][] gameGrid){
 
-        int selectedColumnIndex = playerSelectColumnPrompt(player,gameGrid);
+        boolean turnTaken = false;
+
+        int selectedColumnIndex = playerSelectColumnPrompt(player, gameGrid);
         int[] selectedColumn = gameGrid[selectedColumnIndex];
 
         int playerCounterNumber = player.getCounter().getCounterNumber();
 
-        TurnMechanism.takeTurnDropCounter(playerCounterNumber, selectedColumn);
+            turnTaken = TurnMechanism.takeTurnDropCounter(playerCounterNumber, selectedColumn);
+
+        if(!turnTaken){
+
+            System.out.printf("Please make sure you have selected a column that has space for a counter and is not full. \n");
+            playerTakesTurn(player, gameGrid);
+        }
     }
 
     public static int playerSelectColumnPrompt (Player player, int[][] gameGrid){
@@ -20,7 +28,7 @@ public class Turn {
         int playerCounterNumber = player.getCounter().getCounterNumber();
 
         int minColumnNumber = 0;
-        int maxColumnNumber = gameGrid.length;
+        int maxColumnNumber = gameGrid.length - 1;
 
         System.out.printf("Player %d Select Column:", playerCounterNumber);
         int selectedColumn = ReadInput.readIntFromConsoleInRangeWithPrompt(
