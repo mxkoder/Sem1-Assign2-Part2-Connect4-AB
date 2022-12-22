ae6b3ec15421d6e5e6a7ee682e6a32c3d771a073
Agnes Beviz <Agnes.Beviz@dvla.gov.uk>
2022-12-22 12:09:36 +0000

    Win Condition: refactored row, column and diagonal win conditions

--- src/main/java/org/example/GameWin/WinConditions.java
+++ src/main/java/org/example/GameWin/WinConditions.java
@@ -44,39 +44,30 @@ public class WinConditions {
      * Method to check for a row of 4 identical counters in a game grid
      *
      * @param gameGrid - 2D integer array which represents a game grid in a Connect 4 game. The value '-1' n a cell denotes and empty cell.
-     * @return - Integer - A positive integer with a value representing the counter that has a row of 4, or '-1' if there are no counters with a row of 4 in the grid.
+     * @return - valueOfWinningCounter - A positive integer with a value representing the counter that has a row of 4, or '-1' if there are no counters with a row of 4 in the grid.
      */
     public static int rowOf4Check(int[][] gameGrid) {
 
-        int rowCounter = 1;
+        int valueOfWinningCounter = -1;
 
         // loop through the rows in the game grid
         for (int j = 0; j < gameGrid.length; j++) {
 
-            // loop through the columns in the game grid
-            for (int i = 0; i < gameGrid[0].length - 1; i++) {
+            // loop through the columns on the left side of the game grid
+            for (int i = 0; i <= 2; i++) {
 
-                // for valid counter numbers, checks if two identical counters are next to each other
-                if (gameGrid[i][j] == gameGrid[i + 1][j] && gameGrid[i][j] != -1) {
-
-                    //totals up the number of identical adjacent counters in a row
-                    rowCounter += 1;
-
-                    if (rowCounter == 4) {
-                        return gameGrid[i][j];
-                    }
-
-                } else {
-
-                    rowCounter = 1;
+                // for valid counter numbers, checks if four identical counters are next to each other in a row
+                if (gameGrid[i][j] != -1  &&
+                        gameGrid[i][j] == gameGrid[i + 1][j] &&
+                        gameGrid[i + 1][j] == gameGrid[i + 2][j] &&
+                        gameGrid[i + 2][j] == gameGrid[i + 3][j]
+                ) {
+                    valueOfWinningCounter = gameGrid[i][j];
                 }
             }
-
-            // resets the count for the number of matching adjacent counters if no row of 4 has been found
-            rowCounter = 1;
         }
 
-        return -1;
+        return valueOfWinningCounter;
     }
 
 
@@ -84,38 +75,30 @@ public class WinConditions {
      * Method to check for a column of 4 identical counters in a game grid
      *
      * @param gameGrid - 2D integer array which represents a game grid in a Connect 4 game. The value '-1' n a cell denotes and empty cell.
-     * @return - Integer - A positive integer with a value representing the counter that has a column of 4, or '-1' if there are no counters with a diagonal of 4 in the grid.
+     * @return - valueOfWinningCounter - A positive integer with a value representing the counter that has a column of 4, or '-1' if there are no counters with a diagonal of 4 in the grid.
      */
     public static int columnOf4Check (int[][] gameGrid) {
 
-        int columnCounter = 1;
+        int valueOfWinningCounter = -1;
 
         // loop through the columns in the game grid
         for (int i = 0; i < gameGrid[0].length ; i++) {
 
-            // loop through the columns in the game grid
-            for (int j = 0; j < gameGrid.length - 1; j++) {
+            // loop through the rows in the top half of the game grid
+            for (int j = 0; j <= 2; j++) {
 
-                // for valid counter numbers, checks if two identical counters are next to each other
-                if (gameGrid[i][j] == gameGrid[i][j + 1] && gameGrid[i][j] != -1) {
-
-                    //totals up the number of identical adjacent counters in a row
-                    columnCounter += 1;
-
-                    if (columnCounter == 4) {
-                        return gameGrid[i][j];
-                    }
-
-                } else {
-
-                    columnCounter = 1;
+                // for valid counter numbers, checks if four identical counters are next to each other in a column
+                if (gameGrid[i][j] != -1 &&
+                        gameGrid[i][j] == gameGrid[i][j + 1] &&
+                        gameGrid[i][j + 1] == gameGrid[i][j + 2] &&
+                        gameGrid[i][j + 2] == gameGrid[i][j + 3]
+                ) {
+                    valueOfWinningCounter = gameGrid[i][j];
                 }
             }
-            // resets the count for the number of matching adjacent counters if no column of 4 has been found
-            columnCounter = 1;
         }
 
-        return -1;
+        return valueOfWinningCounter;
     }
 
 
@@ -123,52 +106,40 @@ public class WinConditions {
      * Method to check for a diagonal of 4 identical counters in a  6 x 6 game grid
      *
      * @param gameGrid - 2D 6 x 6 integer array which represents a game grid in a Connect 4 game. The value '-1' n a cell denotes and empty cell.
-     * @return - Integer - A positive integer with a value representing the counter that has a diagonal of 4, or '-1' if there are no counters with a diagonal of 4 in the grid.
+     * @return - valueOfWinningCounter - A positive integer with a value representing the counter that has a diagonal of 4, or '-1' if there are no counters with a diagonal of 4 in the grid.
      */
     public static int diagonalOf4Check (int[][] gameGrid) {
 
-        //-----------------Diagonal 4 in a row checks for diagonals starting in the bottom left corner of the game grid
-        for (int i = 0; i <= 2 ; i++) {
+        int valueOfWinningCounter = -1;
+
+        for (int i = 0; i < gameGrid[0].length ; i++) {
 
             for (int j = 3; j < gameGrid.length; j++) {
 
-                // for valid counter numbers, checks if four identical counters are in a diagonal row
                 if (gameGrid[i][j] != -1) {
 
-                    if (gameGrid[i][j] == gameGrid[i + 1][j - 1] &&
+                    //Diagonal 4 in a row checks for diagonals starting in the bottom left corner of the game grid
+                    if (i <= 2 &&
+                            gameGrid[i][j] == gameGrid[i + 1][j - 1] &&
                             gameGrid[i + 1][j - 1] == gameGrid[i + 2][j - 2] &&
                             gameGrid[i + 2][j - 2] == gameGrid[i + 3][j - 3]
                     ) {
-                        return gameGrid[i][j];
-                    }
-                }
-            }
-        }
+                        valueOfWinningCounter = gameGrid[i][j];
 
-
-        //-----------------Diagonal 4 in a row checks for diagonals starting in the bottom right corner of the game grid
-        for (int i = 3; i < gameGrid[0].length ; i++) {
-
-            for (int j = 3; j < gameGrid.length; j++) {
-
-                // for valid counter numbers, checks if four identical counters are in a diagonal row
-                if (gameGrid[i][j] != -1) {
-
-                    if (gameGrid[i][j] == gameGrid[i - 1][j - 1] &&
+                    //Diagonal 4 in a row checks for diagonals starting in the bottom right corner of the game grid
+                    } else if (i > 2 &&
+                            gameGrid[i][j] == gameGrid[i - 1][j - 1] &&
                             gameGrid[i - 1][j - 1] == gameGrid[i - 2][j - 2] &&
                             gameGrid[i - 2][j - 2] == gameGrid[i - 3][j - 3]
                     ) {
-                        return gameGrid[i][j];
+                        valueOfWinningCounter = gameGrid[i][j];
                     }
                 }
             }
         }
 
-        return -1;
+        return valueOfWinningCounter;
     }
-
-
-
 
 }
 
